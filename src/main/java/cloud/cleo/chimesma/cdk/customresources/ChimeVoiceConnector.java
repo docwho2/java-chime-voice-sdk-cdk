811a9f5e40c95459bf3bd618dcf4d185993dcb2d
Steve Jensen <docwho2@gmail.com>
2023-11-08 05:35:34 -0600

    add PBX_HOSTNAME support for VC

--- src/main/java/cloud/cleo/chimesma/cdk/customresources/ChimeVoiceConnector.java
+++ src/main/java/cloud/cleo/chimesma/cdk/customresources/ChimeVoiceConnector.java
@@ -5,6 +5,7 @@
 package cloud.cleo.chimesma.cdk.customresources;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
@@ -27,20 +28,22 @@ public class ChimeVoiceConnector extends AwsCustomResource {
     private final static String ID = "VC-CR";
 
     /**
+     * If set in the environment, setup Origination to point to it
+     */
+    private final static String PBX_HOSTNAME = System.getenv("PBX_HOSTNAME");
+
+    /**
      * The Voice Connector ID in the API response
      */
     private final static String VC_ID = "VoiceConnector.VoiceConnectorId";
     private final static String VC_ARN = "VoiceConnector.VoiceConnectorArn";
 
-    private final AwsCustomResource logging;
-    private final AwsCustomResource termination;
-    private final AwsCustomResource origination;
-    
+
     public ChimeVoiceConnector(Stack scope) {
         super(scope, ID, AwsCustomResourceProps.builder()
                 .resourceType("Custom::VoiceConnector")
                 .installLatestAwsSdk(Boolean.FALSE)
-                .policy(AwsCustomResourcePolicy.fromStatements(List.of(PolicyStatement.Builder.create().actions(List.of("chime:*","logs:*")).resources(List.of("*")).build())))
+                .policy(AwsCustomResourcePolicy.fromStatements(List.of(PolicyStatement.Builder.create().actions(List.of("chime:*", "logs:*")).resources(List.of("*")).build())))
                 .onCreate(AwsSdkCall.builder()
                         .service("@aws-sdk/client-chime-sdk-voice")
                         .action("CreateVoiceConnectorCommand")
@@ -54,48 +57,58 @@ public class ChimeVoiceConnector extends AwsCustomResource {
                         .build())
                 .build());
 
-        
         // Enable SIP Logs
-        logging = new AwsCustomResource(scope, ID + "-LOG", AwsCustomResourceProps.builder()
+        final var logging = new AwsCustomResource(scope, ID + "-LOG", AwsCustomResourceProps.builder()
                 .resourceType("Custom::VoiceConnectorLogging")
-                 .installLatestAwsSdk(Boolean.FALSE)
-                .policy(AwsCustomResourcePolicy.fromStatements(List.of(PolicyStatement.Builder.create().actions(List.of("chime:*","logs:*")).resources(List.of("*")).build())))
+                .installLatestAwsSdk(Boolean.FALSE)
+                .policy(AwsCustomResourcePolicy.fromStatements(List.of(PolicyStatement.Builder.create().actions(List.of("chime:*", "logs:*")).resources(List.of("*")).build())))
                 .onCreate(AwsSdkCall.builder()
                         .service("@aws-sdk/client-chime-sdk-voice")
                         .action("PutVoiceConnectorLoggingConfigurationCommand")
                         .physicalResourceId(PhysicalResourceId.of("logging"))
                         .parameters(Map.of("VoiceConnectorId", getResponseFieldReference(VC_ID),
-                                "LoggingConfiguration",Map.of("EnableSIPLogs", true,"EnableMediaMetricLogs",false)))
+                                "LoggingConfiguration", Map.of("EnableSIPLogs", true, "EnableMediaMetricLogs", false)))
                         .build())
                 .build());
-        
-        
-        termination = new AwsCustomResource(scope, ID + "-TERM", AwsCustomResourceProps.builder()
+
+        // Start with list of Twilio NA ranges for SIP Trunking
+        var cidrAllowList = List.of("54.172.60.0/30", "54.244.51.0/30");
+        if (PBX_HOSTNAME != null) {
+            cidrAllowList = new ArrayList(cidrAllowList);
+            cidrAllowList.add(PBX_HOSTNAME);
+        }
+
+        final var termination = new AwsCustomResource(scope, ID + "-TERM", AwsCustomResourceProps.builder()
                 .resourceType("Custom::VoiceConnectorTerm")
-                 .installLatestAwsSdk(Boolean.FALSE)
+                .installLatestAwsSdk(Boolean.FALSE)
                 .policy(AwsCustomResourcePolicy.fromSdkCalls(SdkCallsPolicyOptions.builder().resources(AwsCustomResourcePolicy.ANY_RESOURCE).build()))
                 .onCreate(AwsSdkCall.builder()
                         .service("@aws-sdk/client-chime-sdk-voice")
                         .action("PutVoiceConnectorTerminationCommand")
                         .physicalResourceId(PhysicalResourceId.of("termination"))
                         .parameters(Map.of("VoiceConnectorId", getResponseFieldReference(VC_ID),
-                                "Termination",Map.of("CallingRegions", List.of("US"),"CidrAllowedList",List.of("54.197.158.204/32","54.172.60.0/30","54.244.51.0/30"),"Disabled",false)))
+                                "Termination", Map.of("CallingRegions", List.of("US"), "CidrAllowedList", cidrAllowList, "Disabled", false)))
                         .build())
                 .build());
-        
-        origination = new AwsCustomResource(scope, ID + "-ORIG", AwsCustomResourceProps.builder()
-                .resourceType("Custom::VoiceConnectorOrig")
-                 .installLatestAwsSdk(Boolean.FALSE)
-                .policy(AwsCustomResourcePolicy.fromSdkCalls(SdkCallsPolicyOptions.builder().resources(AwsCustomResourcePolicy.ANY_RESOURCE).build()))
-                .onCreate(AwsSdkCall.builder()
-                        .service("@aws-sdk/client-chime-sdk-voice")
-                        .action("PutVoiceConnectorOriginationCommand")
-                        .physicalResourceId(PhysicalResourceId.of("origination"))
-                        .parameters(Map.of("VoiceConnectorId", getResponseFieldReference(VC_ID),
-                                "Origination",Map.of("Routes", List.of(Map.of("Host","54.197.158.204","Port",5060,"Protocol","UDP","Priority",1,"Weight",1)),"Disabled",false)))
-                        .build())
-                .build());
-        
+
+        /**
+         * Only need to configure origination if outbound calls are needed for SIP
+         */
+        if (PBX_HOSTNAME != null) {
+            final var origination = new AwsCustomResource(scope, ID + "-ORIG", AwsCustomResourceProps.builder()
+                    .resourceType("Custom::VoiceConnectorOrig")
+                    .installLatestAwsSdk(Boolean.FALSE)
+                    .policy(AwsCustomResourcePolicy.fromSdkCalls(SdkCallsPolicyOptions.builder().resources(AwsCustomResourcePolicy.ANY_RESOURCE).build()))
+                    .onCreate(AwsSdkCall.builder()
+                            .service("@aws-sdk/client-chime-sdk-voice")
+                            .action("PutVoiceConnectorOriginationCommand")
+                            .physicalResourceId(PhysicalResourceId.of("origination"))
+                            .parameters(Map.of("VoiceConnectorId", getResponseFieldReference(VC_ID),
+                                    "Origination", Map.of("Routes", List.of(Map.of("Host", PBX_HOSTNAME, "Port", 5060, "Protocol", "UDP", "Priority", 1, "Weight", 1)), "Disabled", false)))
+                            .build())
+                    .build());
+        }
+
     }
 
     /**
@@ -106,7 +119,7 @@ public class ChimeVoiceConnector extends AwsCustomResource {
     public String getArn() {
         return getResponseField(VC_ARN);
     }
-    
+
     public String getOutboundName() {
         return getResponseField("VoiceConnector.OutboundHostName");
     }
@@ -115,7 +128,7 @@ public class ChimeVoiceConnector extends AwsCustomResource {
      * Required parameters for the CreateVoiceConnectorCommand API call
      */
     @AllArgsConstructor
-    private static class VCParameters  {
+    private static class VCParameters {
 
         @JsonProperty(value = "AwsRegion")
         String awsRegion;
