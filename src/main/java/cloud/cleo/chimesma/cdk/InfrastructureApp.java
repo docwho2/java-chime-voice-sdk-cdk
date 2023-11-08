ae55eb661c75df2d69972972e9760aec905d1014
Steve Jensen <docwho2@gmail.com>
2023-11-08 04:50:00 -0600

    cleanup deploy actions

--- src/main/java/cloud/cleo/chimesma/cdk/InfrastructureApp.java
+++ src/main/java/cloud/cleo/chimesma/cdk/InfrastructureApp.java
@@ -11,24 +11,26 @@ public final class InfrastructureApp {
     public static void main(final String[] args) {
         final var app = new App();
 
+        // Required Param
         String accountId = (String) app.getNode().tryGetContext("accountId");
         requireNonEmpty(accountId, "accountId is required via -c parameter to cdk");
         
-        String stackName = (String) app.getNode().tryGetContext("stackName");
-        if ( stackName == null || stackName.isBlank() ) {
-            stackName = "chime-sdk-cdk-provisioning";
-        }
+        // Optional Params
+        String stackName = getParamOrDefault(app, "stackName", "chime-sdk-cdk-provisioning");
+        String regionEast = getParamOrDefault(app, "regionEast", "us-east-1");
+        String regionWest = getParamOrDefault(app, "regionWest", "us-west-2");
+        
 
         final var stack_east = new InfrastructureStack(app, "east", StackProps.builder()
                 .description(STACK_DESC)
                 .stackName(stackName)
-                .env(makeEnv(accountId, "us-east-1"))
+                .env(makeEnv(accountId, regionEast))
                 .build());
         
         final var stack_west = new InfrastructureStack(app, "west", StackProps.builder()
                 .description(STACK_DESC)
                 .stackName(stackName)
-                .env(makeEnv(accountId, "us-west-2"))
+                .env(makeEnv(accountId, regionWest))
                 .build());
 
         app.synth();
@@ -42,6 +44,11 @@ public final class InfrastructureApp {
                 .build();
     }
 
+    static String getParamOrDefault(App app, String param, String defaultValue) {
+        final var val = (String) app.getNode().tryGetContext(param);
+         return val == null || val.isBlank() ? defaultValue : val;
+    }
+    
     static void requireNonEmpty(String string, String message) {
         if (string == null || string.isBlank()) {
             throw new IllegalArgumentException(message);
