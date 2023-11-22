24eff2219730022298fec709316f1c7b61a0e276
Steve Jensen <docwho2@gmail.com>
2023-11-22 08:18:37 -0600

    Fix CloudShell deploy issues

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,19 @@
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
+if [ "$AWS_EXECUTION_ENV" = "CloudShell" ]; then
+    echo "CloudShell Detected, installing Java and Maven dependency"
+    # Install needed tools
+    sudo yum -y install java-17-amazon-corretto
+    # Ensure we are on latest CDK
+    sudo npm install -g aws-cdk
+    # Install 3.9.5 Maven Version which we know works (yum package is too outdated)
+    pushd ".."
+    wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.tar.gz
+    tar -xvf apache-maven-3.9.5-bin.tar.gz
+    rm -f apache-maven-3.9.5-bin.tar.gz
+    export PATH="/home/cloudshell-user/apache-maven-3.9.5/bin:${PATH}"
+    popd
+fi
 
 cdk deploy -c accountId=${ACCOUNT_ID} -c stackName=${CDK_STACK_NAME} -c regionEast=${regions[0]} -c regionWest=${regions[1]} --concurrency=4 --all --require-approval=never
